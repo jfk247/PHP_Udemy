be9a8a12a0f235730a8051bd462e3df0e00b02e1
John Kennedy <kennedy.john1@gmail.com>
2018-06-22 11:33:08 +0000

    updated index page

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
+<html>
+    
+    <head>
+        <title>This is my php website</title>
+    </head>
+</html>
+<body>
+<center>
+    <h1>
 <?php
     echo "Welcome, I am connecting Android to PHP, MySQL";
+    ?>
+    <br>
+    <?php
 	echo "This is my test index page";
 ?>
+</h1>
+</center>
+</body>
